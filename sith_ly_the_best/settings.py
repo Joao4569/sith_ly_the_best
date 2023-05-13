a5c36bc7a380f1259f1cb37b94e829a6c080c122
Joao4569 <johnnyrod4569@gmail.com>
2023-05-13 11:11:59 +0000

    feat: Prepared environment and settings.py file for deployment

--- sith_ly_the_best/settings.py
+++ sith_ly_the_best/settings.py
@@ -11,6 +11,13 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+# Imports for deployment - Johnny
+import os
+import dj_database_url
+
+# Code needed for deployment - Johnny
+if os.path.isfile("env.py"):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4+_%p_&we#govk2xdh$yi12m4==m3etccxyn&zhay+$*7%tlox'
+# Removed insecure secret key and replaced it with a secure one - Johnny
+# Insecure key = 'django-insecure-4+_%p_&we#govk2xdh$yi12m4==m3etccxyn&zhay+$*7%tlox'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -73,13 +82,18 @@ WSGI_APPLICATION = 'sith_ly_the_best.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# Old databases need to be commented out - Johnny
+# DATABASES = {
+#   'default': {
+#       'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+# }
 
+
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
