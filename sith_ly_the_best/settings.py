8e9d3b4e2432f42fcfbb5bc6c052197ad6fc40de
Joao4569 <johnnyrod4569@gmail.com>
2023-05-15 08:13:58 +0000

    Remove second import of os

--- sith_ly_the_best/settings.py
+++ sith_ly_the_best/settings.py
@@ -19,7 +19,6 @@ import dj_database_url
 if os.path.isfile("env.py"):
     import env
 
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
