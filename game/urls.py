f1c26515773ce6b961d153d790b7b33a06648e29
Joao4569 <johnnyrod4569@gmail.com>
2023-05-15 09:08:08 +0000

    chore: Add comments to Url's and authentication backends

--- game/urls.py
+++ game/urls.py
@@ -4,7 +4,9 @@ from . import views
 
 
 urlpatterns = [
+    # Path to game Homepage
     path('', views.homepage, name='homepage'),
+    
     path('game/', views.game_view, name='game'),
     path('save_score/', views.save_score, name='save_score'),
     path('leaderboard/', views.leaderboard, name='leaderboard'),
